7ec066f531ab6f0d7a9b84c504b90bff0aba350d
Thabiso Gift Ramolobeng <tgramdev001@gmail.com>
2024-06-09 20:44:06 +0200

    Categories page added

---
+++ MealsXamarinFormsApp/Views/CategoriesPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MealsXamarinFormsApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MealsXamarinFormsApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CategoriesPage : ContentPage
+    {
+        private readonly CategoryViewModel _viewModel;
+        public CategoriesPage()
+        {
+            InitializeComponent();
+            _viewModel = App.ServiceProvider.GetService<CategoryViewModel>();
+            BindingContext = _viewModel;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await _viewModel.LoadCategoriesAsync();
+        }
+    }
+}
