5db2fae877366c2a722ffcd2056301eb5e70cee4
Thabiso Gift Ramolobeng <tgramdev001@gmail.com>
2024-06-09 19:43:50 +0200

    View models and data source

---
+++ MealsXamarinFormsApp/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,46 @@
+﻿using MealsXamarinFormsApp.Models;
+using MealsXamarinFormsApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MealsXamarinFormsApp.ViewModels
+{
+    public class CategoryViewModel : BindableObject
+    {
+
+        private ObservableCollection<Category> _categories;
+        public ObservableCollection<Category> Categories
+        {
+            get => _categories;
+            set
+            {
+                _categories = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private readonly IMealService _mealService;
+
+        public CategoryViewModel(IMealService mealService)
+        {
+            _mealService = mealService;
+            Categories = new ObservableCollection<Category>();
+        }
+
+        public async Task LoadCategoriesAsync()
+        {
+            var categoryResponse = await _mealService.GetCategoriesAsync();
+            if (categoryResponse?.Categories != null)
+            {
+                foreach (var category in categoryResponse.Categories)
+                {
+                    Categories.Add(category);
+                }
+            }
+        }
+    }
+}
